f57e47ec40a6109cd746021a5bec453743f9d39d
gokhan0706 <57571309+gokhan0706@users.noreply.github.com>
2022-05-17 00:17:34 +0200

    Update MainClass.java

--- src/JavaDers65/MainClass.java
+++ src/JavaDers65/MainClass.java
@@ -8,7 +8,7 @@ Ogretmen nesne = new Ogretmen();
 
 nesne.setYas(23); // set methodu yardimi ile degerleri degistirebilirim
 nesne.getYas(); //  Get methodu yardimi ile datayi cagirabilirim ama degisitiremem
-nesne.getIsim();//Kullanicni sadec adii gorebilirm ama degistiremem
+nesne.getIsim();//Kullanicni sadec adii gorebilirm ama degistiremem   mrehaba
 
     }
 }
